6d1b0b3a6861d197c44ca61ddc94862f920db86c
sovrasov <sovrasov.vlad@gmail.com>
2016-12-19 23:57:07 +0300

    Add tests for viewModel with fake logger

---
+++ code/todo-list/ViewModel/src/test/java/ru/unn/agile/todoapp/viewmodel/TodoAppViewModelLoggingTest.java
@@ -0,0 +1,35 @@
+package ru.unn.agile.todoapp.viewmodel;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class TodoAppViewModelLoggingTest {
+    private static final LocalDate TODAY = LocalDate.now();
+    private static final String TEST_TASK_DESCRIPTION = "Test task";
+    private TodoAppViewModel viewModel;
+
+    @Before
+    public void setUp() {
+        viewModel = new TodoAppViewModel(new TestLogger());
+    }
+
+    @Test
+    public void logIsEmptyAfterStart() {
+        List<String> log = viewModel.getLog();
+        assertEquals(true, log.isEmpty());
+    }
+
+    @Test
+    public void setNewTaskDescriptionMentionedInTheLog()  {
+        viewModel.setNewTaskDescription("Test task");
+        String lastLogMessage = viewModel.getLastLogString();
+        viewModel.onNewTaskDescriptinChanged();
+        assertTrue(lastLogMessage.matches(".*" + viewModel.getNewTaskDescription() + ".*"));
+    }
+}
