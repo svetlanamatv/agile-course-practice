a76dc7b45dca75f4c2def7fa94c53b4550990d36
ElanovArtyom <elanoff.artyom@yandex.ru>
2016-12-21 01:26:03 +0300

    edit

--- code/ElanovArtyom/src/main/java/ru/unn/agile/IntersectTwoLine/Formatter.java
+++ code/ElanovArtyom/src/main/java/ru/unn/agile/IntersectTwoLine/Formatter.java
@@ -13,20 +13,20 @@ public final class Formatter {
         }
     }
 
-    public static String getFormatted(final LineSegment2D LineSegment) {
+    public static String getFormatted(final LineSegment2D lineSegment) {
         StringBuffer buffer = new StringBuffer();
-        if (LineSegment.getA()==0.0 || LineSegment.getB()==0.0) {
+        if (lineSegment.getA() == 0.0 || lineSegment.getB() == 0.0) {
             throw new IllegalArgumentException("arguments A and B can not be null");
         }
             else {
-            double A = formatPositiveDouble(LineSegment.getA());
-            buffer.append(LineSegment.getA() < 0 ? "-" : "");
+            double A = formatPositiveDouble(lineSegment.getA());
+            buffer.append(lineSegment.getA() < 0 ? "-" : "");
             buffer.append(A + "*x");
-            double B = formatPositiveDouble(LineSegment.getB());
-            buffer.append(LineSegment.getB() < 0 ? "-" : "+");
+            double B = formatPositiveDouble(lineSegment.getB());
+            buffer.append(lineSegment.getB() < 0 ? "-" : "+");
             buffer.append(B + "*y");
-            double C = formatPositiveDouble(LineSegment.getC());
-            buffer.append(LineSegment.getC() < 0 ? "-" : "+");
+            double C = formatPositiveDouble(lineSegment.getC());
+            buffer.append(lineSegment.getC() < 0 ? "-" : "+");
             buffer.append(C);
             buffer.append("=0");
             return buffer.toString();
