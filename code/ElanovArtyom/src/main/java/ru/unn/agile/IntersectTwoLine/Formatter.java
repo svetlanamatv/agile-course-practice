9249d9954e00d1db0df86f777efde10db8d39f25
ElanovArtyom <elanoff.artyom@yandex.ru>
2016-12-21 01:13:08 +0300

    Edit Formatter
    CheckStyle error editing

--- code/ElanovArtyom/src/main/java/ru/unn/agile/IntersectTwoLine/Formatter.java
+++ code/ElanovArtyom/src/main/java/ru/unn/agile/IntersectTwoLine/Formatter.java
@@ -1,30 +1,32 @@
 package ru.unn.agile.IntersectTwoLine;
 
-/**
- * Created by User on 020 20.12.16.
- */
 public final class Formatter {
 
+    private Formatter() { }
+
     public static double formatPositiveDouble(final double value) {
-        if (value <0)
+        if (value < 0) {
             return -value;
-        else
+        }
+        else {
             return value;
+        }
     }
 
-    public static String getFormatted(final LineSegment2D Line) {
+    public static String getFormatted(final LineSegment2D LineSegment) {
         StringBuffer buffer = new StringBuffer();
-        if (Line.getA()==0.0 || Line.getB()==0.0)
-        throw new IllegalArgumentException("arguments A and B can not be null");
-            else{
-            double A = formatPositiveDouble(Line.getA());
-            buffer.append(Line.getA() < 0 ? "-" : "");
+        if (LineSegment.getA()==0.0 || LineSegment.getB()==0.0) {
+            throw new IllegalArgumentException("arguments A and B can not be null");
+        }
+            else {
+            double A = formatPositiveDouble(LineSegment.getA());
+            buffer.append(LineSegment.getA() < 0 ? "-" : "");
             buffer.append(A + "*x");
-            double B = formatPositiveDouble(Line.getB());
-            buffer.append(Line.getB() < 0 ? "-" : "+");
+            double B = formatPositiveDouble(LineSegment.getB());
+            buffer.append(LineSegment.getB() < 0 ? "-" : "+");
             buffer.append(B + "*y");
-            double C = formatPositiveDouble(Line.getC());
-            buffer.append(Line.getC() < 0 ? "-" : "+");
+            double C = formatPositiveDouble(LineSegment.getC());
+            buffer.append(LineSegment.getC() < 0 ? "-" : "+");
             buffer.append(C);
             buffer.append("=0");
             return buffer.toString();
