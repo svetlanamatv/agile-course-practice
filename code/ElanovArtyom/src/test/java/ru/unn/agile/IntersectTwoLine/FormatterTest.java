6831175ac299073e6f57878bd3ca91a377cf4ed5
ElanovArtyom <elanoff.artyom@yandex.ru>
2016-12-21 12:03:26 +0300

    Edit style test

--- code/ElanovArtyom/src/test/java/ru/unn/agile/IntersectTwoLine/FormatterTest.java
+++ code/ElanovArtyom/src/test/java/ru/unn/agile/IntersectTwoLine/FormatterTest.java
@@ -4,49 +4,45 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by User on 020 20.12.16.
- */
 public class FormatterTest {
 
     @Test
-    public void canFormatToFormulaForDefaultLine(){
-        LineSegment2D LineSegment = new LineSegment2D (2.1,3.1,4.1);
-        assertEquals("2.1*x+3.1*y+4.1=0", LineSegment.toFormula());
+    public void canFormatToFormulaForDefaultLine() {
+        LineSegment2D lineSegment = new LineSegment2D(2.1, 3.1, 4.1);
+        assertEquals("2.1*x+3.1*y+4.1=0", lineSegment.toFormula());
     }
     @Test
-    public void canFormatToFormulaWithAllNegativeArguments(){
-        LineSegment2D LineSegment = new LineSegment2D (-2.1,-3.1,-4.1);
-        assertEquals("-2.1*x-3.1*y-4.1=0", LineSegment.toFormula());
+    public void canFormatToFormulaWithAllNegativeArguments() {
+        LineSegment2D lineSegment = new LineSegment2D(-2.1, -3.1, -4.1);
+        assertEquals("-2.1*x-3.1*y-4.1=0", lineSegment.toFormula());
+    }
+    @Test
+    public void canFormatToFormulaWithNegativeArgumentA() {
+        LineSegment2D lineSegment = new LineSegment2D(-2.1, 3.1, 4.1);
+        assertEquals("-2.1*x+3.1*y+4.1=0", lineSegment.toFormula());
 
     }
     @Test
-    public void canFormatToFormulaWithNegativeArgumentA(){
-        LineSegment2D LineSegment = new LineSegment2D (-2.1,3.1,4.1);
-        assertEquals("-2.1*x+3.1*y+4.1=0", LineSegment.toFormula());
+    public void canFormatToFormulaWithNegativeArgumentB() {
+        LineSegment2D lineSegment = new LineSegment2D(2.1, -3.1, 4.1);
+        assertEquals("2.1*x-3.1*y+4.1=0", lineSegment.toFormula());
 
     }
     @Test
-    public void canFormatToFormulaWithNegativeArgumentB(){
-        LineSegment2D LineSegment = new LineSegment2D (2.1,-3.1,4.1);
-        assertEquals("2.1*x-3.1*y+4.1=0", LineSegment.toFormula());
-
-    }
-    @Test
-    public void canFormatToFormulaWithNegativeArgumentC(){
-        LineSegment2D LineSegment = new LineSegment2D (2.1,3.1,-4.1);
-        assertEquals("2.1*x+3.1*y-4.1=0", LineSegment.toFormula());
+    public void canFormatToFormulaWithNegativeArgumentC() {
+        LineSegment2D lineSegment = new LineSegment2D(2.1, 3.1, -4.1);
+        assertEquals("2.1*x+3.1*y-4.1=0", lineSegment.toFormula());
 
     }
     @Test (expected = IllegalArgumentException.class)
-    public void cannotFormatToFormulaWithZeroArgumentA(){
-        LineSegment2D LineSegment = new LineSegment2D (0,3.1,-4.1);
-        LineSegment.toFormula();
+    public void cannotFormatToFormulaWithZeroArgumentA() {
+        LineSegment2D lineSegment = new LineSegment2D(0, 3.1, -4.1);
+        lineSegment.toFormula();
     }
     @Test (expected = IllegalArgumentException.class)
-    public void cannotFormatToFormulaWithZeroArgumentB(){
-        LineSegment2D LineSegment = new LineSegment2D (2.1,0,-4.1);
-        LineSegment.toFormula();
+    public void cannotFormatToFormulaWithZeroArgumentB() {
+        LineSegment2D lineSegment = new LineSegment2D(2.1, 0, -4.1);
+        lineSegment.toFormula();
     }
 
 }
--- code/ElanovArtyom/src/test/java/ru/unn/agile/IntersectTwoLine/IntersectTwoLineTest.java
+++ code/ElanovArtyom/src/test/java/ru/unn/agile/IntersectTwoLine/IntersectTwoLineTest.java
@@ -12,43 +12,43 @@ public class IntersectTwoLineTest {
 
     @Test
     public void canCreateLineSegmentWithInitialValues() {
-        LineSegment2D LineSegment = new LineSegment2D(2.1, 3.2, 4.5);
-        assertNotNull(LineSegment);
+        LineSegment2D lineSegment = new LineSegment2D(2.1, 3.2, 4.5);
+        assertNotNull(lineSegment);
     }
     @Test
     public void canSetInitialAValue() {
-        LineSegment2D LineSegment = new LineSegment2D(2.1, 3.2, 4.5);
-        assertEquals(2.1, LineSegment.getA(), delta);
+        LineSegment2D lineSegment = new LineSegment2D(2.1, 3.2, 4.5);
+        assertEquals(2.1, lineSegment.getA(), delta);
     }
     @Test
     public void canSetInitialBValue() {
-        LineSegment2D LineSegment = new LineSegment2D(2.1, 3.2, 4.5);
-        assertEquals(3.2, LineSegment.getB(), delta);
+        LineSegment2D lineSegment = new LineSegment2D(2.1, 3.2, 4.5);
+        assertEquals(3.2, lineSegment.getB(), delta);
     }
     @Test
     public void canSetInitialCValue() {
-        LineSegment2D LineSegment = new LineSegment2D(2.1, 3.2, 4.5);
-        assertEquals(4.5, LineSegment.getC(), delta);
+        LineSegment2D lineSegment = new LineSegment2D(2.1, 3.2, 4.5);
+        assertEquals(4.5, lineSegment.getC(), delta);
     }
 
     @Test
     public void canCreatePointIntersectTwoLineSegment() {
-        LineSegment2D LineSegment1 = new LineSegment2D(3.1, -2.3, 3);
-        LineSegment2D LineSegment2 = new LineSegment2D(-4.5, 5.6, -1);
-        assertNotNull(LineSegment1.checkIntersection(LineSegment2));
+        LineSegment2D lineSegment1 = new LineSegment2D(3.1, -2.3, 3);
+        LineSegment2D lineSegment2 = new LineSegment2D(-4.5, 5.6, -1);
+        assertNotNull(lineSegment1.checkIntersection(lineSegment2));
     }
 
     @Test
     public void showThatLinesAreParallel() {
-        LineSegment2D LineSegment1 = new LineSegment2D(2.1, 3.1, -1);
-        LineSegment2D LineSegment2 = new LineSegment2D(4.2, 6.2, 7);
-        assertEquals( "Lines are parallel", LineSegment1.checkIntersection(LineSegment2));
+        LineSegment2D lineSegment1 = new LineSegment2D(2.1, 3.1, -1);
+        LineSegment2D lineSegment2 = new LineSegment2D(4.2, 6.2, 7);
+        assertEquals("Lines are parallel", lineSegment1.checkIntersection(lineSegment2));
     }
     @Test
     public void showThatLinesMatch() {
-        LineSegment2D LineSegment1 = new LineSegment2D(2.1, 3.1, -1);
-        LineSegment2D LineSegment2 = new LineSegment2D(4.2, 6.2, -2);
-        assertEquals( "Lines math", LineSegment1.checkIntersection(LineSegment2));
+        LineSegment2D lineSegment1 = new LineSegment2D(2.1, 3.1, -1);
+        LineSegment2D lineSegment2 = new LineSegment2D(4.2, 6.2, -2);
+        assertEquals("Lines math", lineSegment1.checkIntersection(lineSegment2));
     }
 
 
