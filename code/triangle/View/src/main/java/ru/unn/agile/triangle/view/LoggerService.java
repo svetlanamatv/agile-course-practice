45ac41a6edd05da7c79ea839a630f34d2c7492eb
Ruslan Israfilov <ruslan.is@outlook.com>
2016-12-25 00:14:07 +0300

    Inject plain text logger to View assembly

---
+++ code/triangle/View/src/main/java/ru/unn/agile/triangle/view/LoggerService.java
@@ -0,0 +1,24 @@
+package ru.unn.agile.triangle.view;
+
+import ru.unn.agile.triangle.infrastructure.PlainTextFileLogger;
+import ru.unn.agile.triangle.logging.Logger;
+
+import java.io.IOException;
+
+class LoggerService {
+    private static final String LOGGER_FILE_NAME = "./triangle-app.log";
+    private static final int MAX_RECORDS_IN_MEMORY = 100;
+
+    static Logger getLogger() {
+        try {
+            return new PlainTextFileLogger(
+                    LOGGER_FILE_NAME,
+                    MAX_RECORDS_IN_MEMORY);
+        } catch (final IOException ioex) {
+            System.out.format("Can't create text file logger with output file %s,"
+                    + "application will start but without logging capability",
+                    LOGGER_FILE_NAME);
+            return new StubLogger();
+        }
+    }
+}
---
+++ code/triangle/View/src/main/java/ru/unn/agile/triangle/view/StubLogger.java
@@ -0,0 +1,29 @@
+package ru.unn.agile.triangle.view;
+
+import ru.unn.agile.triangle.logging.Logger;
+import ru.unn.agile.triangle.logging.LoggerRecord;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class StubLogger implements Logger {
+    @Override
+    public void print(final String message) {
+        // Stub
+    }
+
+    @Override
+    public void print(final String pattern, final Object... args) {
+        // Stub
+    }
+
+    @Override
+    public int getRecordsNumber() {
+        return 0;
+    }
+
+    @Override
+    public List<LoggerRecord> getLastRecords(final int recordsNumber) {
+        return new ArrayList<>();
+    }
+}
