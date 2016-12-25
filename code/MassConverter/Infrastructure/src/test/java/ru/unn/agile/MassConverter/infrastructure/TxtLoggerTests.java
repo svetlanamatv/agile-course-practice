0dd2af8b72ba36870b01673197add3c5168eeada
Nikita-K <n.yu.kudriavtsev@gmail.com>
2016-12-25 13:09:09 +0300

    Added TxtLogger

---
+++ code/MassConverter/Infrastructure/src/test/java/ru/unn/agile/MassConverter/infrastructure/TxtLoggerTests.java
@@ -0,0 +1,71 @@
+package ru.unn.agile.MassConverter.infrastructure;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
+import static junit.framework.TestCase.assertNotNull;
+
+public class TxtLoggerTests {
+    private static final String FILENAME = "./TxtLoggerTests-lab3.log";
+    private TxtLogger txtLogger;
+
+    @Before
+    public void setUp() {
+        txtLogger = new TxtLogger(FILENAME);
+    }
+
+    @Test
+    public void canCreateLoggerWithFileName() {
+        assertNotNull(txtLogger);
+    }
+
+    @Test
+    public void canCreateLogFileOnDisk() {
+        try {
+            new BufferedReader(new FileReader(FILENAME));
+        } catch (FileNotFoundException e) {
+            fail("File " + FILENAME + " wasn't found!");
+        }
+    }
+
+    @Test
+    public void canWriteLogMessage() {
+        String testMessage = "Test message";
+
+        txtLogger.log(testMessage);
+
+        String message = txtLogger.getLog().get(0);
+        assertTrue(message.matches(".*" + testMessage + ".*"));
+    }
+
+    @Test
+    public void canWriteSeveralLogMessage() {
+        String[] messages = {"Test message 1", "Test message 2"};
+
+        txtLogger.log(messages[0]);
+        txtLogger.log(messages[1]);
+
+        List<String> actualMessages = txtLogger.getLog();
+        for (int i = 0; i < actualMessages.size(); i++) {
+            assertTrue(actualMessages.get(i).matches(".*" + messages[i] + ".*"));
+        }
+    }
+
+    @Test
+    public void doesLogContainDateAndTime() {
+        String testMessage = "Test message";
+
+        txtLogger.log(testMessage);
+
+        String message = txtLogger.getLog().get(0);
+        assertTrue(message.matches("^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2} > .*"));
+    }
+}
