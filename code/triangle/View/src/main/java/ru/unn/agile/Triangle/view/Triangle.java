8e8f0deab4d5f6dd555154f72a5c94300239a1b7
Ruslan Israfilov <ruslan.is@outlook.com>
2016-12-25 14:41:09 +0300

    Add binding to logger records

--- code/triangle/View/src/main/java/ru/unn/agile/Triangle/view/Triangle.java
+++ code/triangle/View/src/main/java/ru/unn/agile/Triangle/view/Triangle.java
@@ -1,13 +1,20 @@
 package ru.unn.agile.triangle.view;
 
+import javafx.beans.property.ReadOnlyListProperty;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import ru.unn.agile.triangle.viewmodel.LoggerRecordViewModel;
 import ru.unn.agile.triangle.viewmodel.ViewModel;
 
 public class Triangle {
+    @FXML
+    private ListView logList;
     @FXML
     private ViewModelProvider viewModelProvider;
     @FXML
@@ -40,6 +47,15 @@ public class Triangle {
             @Override
             public void handle(final ActionEvent event) {
                 viewModel.calculate();
+            }
+        });
+
+        ReadOnlyListProperty<LoggerRecordViewModel> records =
+                viewModel.getLoggerViewModel().recordsProperty();
+        records.addListener(new ListChangeListener<LoggerRecordViewModel>() {
+            @Override
+            public void onChanged(Change<? extends LoggerRecordViewModel> change) {
+                logList.scrollTo(logList.getItems().size() - 1);
             }
         });
     }
