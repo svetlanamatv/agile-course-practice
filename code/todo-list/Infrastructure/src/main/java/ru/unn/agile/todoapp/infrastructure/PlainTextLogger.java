9d454346509051c715c5164a96d6f80dd64d2e37
sovrasov <sovrasov.vlad@gmail.com>
2016-12-28 15:10:53 +0300

    Add txt logger implementation

---
+++ code/todo-list/Infrastructure/src/main/java/ru/unn/agile/todoapp/infrastructure/PlainTextLogger.java
@@ -0,0 +1,58 @@
+package ru.unn.agile.todoapp.infrastructure;
+
+import  ru.unn.agile.todoapp.viewmodel.ILogger;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+public class PlainTextLogger implements ILogger {
+    private static final String DATE_FORMAT_NOW = "yyyy-MM-dd HH:mm:ss";
+    private final BufferedWriter writer;
+    private final ArrayList<String> inMemoryLog = new ArrayList<>();
+
+    private static String getCurrentTime() {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW, Locale.ENGLISH);
+        return sdf.format(cal.getTime());
+    }
+
+    public PlainTextLogger(final String pathToLog)  {
+        BufferedWriter logWriter = null;
+        try {
+            logWriter = new BufferedWriter(new FileWriter(pathToLog));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        writer = logWriter;
+    }
+
+    @Override
+    public void addToLog(final String message) {
+        String logMessage = getCurrentTime() + " > " + message;
+
+        inMemoryLog.add(logMessage);
+
+        try {
+            writer.write(logMessage);
+            writer.newLine();
+            writer.flush();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public List<String> getLog() {
+        return inMemoryLog;
+    }
+
+    @Override
+    public String getLastLogMessage()  {
+        return inMemoryLog.get(inMemoryLog.size() - 1);
+    };
+}
