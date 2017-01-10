5ed2e23c8b3375801bf593b39effd1c0dc06e4b2
Karchkov_Denis <karchkov.denis@mail.ru>
2017-01-10 12:26:29 +0300

    New commit

--- code/matrixdiff/ViewModel/src/test/java/com/github/audice/matrixdiff/viewmodel/MatrixDiffViewModelTest.java
+++ code/matrixdiff/ViewModel/src/test/java/com/github/audice/matrixdiff/viewmodel/MatrixDiffViewModelTest.java
@@ -1,68 +1,74 @@
 package com.github.audice.matrixdiff.viewmodel;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 /**
- * Created by Denis on 23.12.2016.
- */
+* Created by Denis on 23.12.2016.
+*/
 public class MatrixDiffViewModelTest {
-    @Test
-    public void byDefaultToFillButtonIsDisabled() {
-        MatrixDiffViewModel viewModel = new MatrixDiffViewModel();
-        assertFalse(viewModel.isToFillButtonEnabled());
-    }
+private MatrixDiffViewModel viewModel;
 
-    @Test
-    public void whenEnteredCorrectNumericInTextFieldToFillButtonIsEnabled() {
-        MatrixDiffViewModel viewModel = new MatrixDiffViewModel();
-        viewModel.setSizeOfMatrix("3");
-        assertTrue(viewModel.isToFillButtonEnabled());
-    }
+@Before
+public void enteringDate() {
+viewModel = new MatrixDiffViewModel();
+}
 
-    @Test
-    public void whenClearTextFieldToFillButtonIsDisabled() {
-        MatrixDiffViewModel viewModel = new MatrixDiffViewModel();
-        viewModel.setSizeOfMatrix("3");
-        viewModel.setSizeOfMatrix("");
-        assertFalse(viewModel.isToFillButtonEnabled());
-    }
+@After
+public void tearDown() {
+viewModel = null;
+}
 
-    @Test
-    public void whenEnterRightSizeGetImplementOfMatrix() {
-        MatrixDiffViewModel viewModel = new MatrixDiffViewModel();
-        viewModel.setSizeOfMatrix("3");
-        assertNotEquals(viewModel.getEmptyMatrix(), "");
-    }
+@Test
+public void byDefaultToFillButtonIsDisabled() {
+assertFalse(viewModel.isToFillButtonEnabled());
+}
 
-    @Test
-    public void whenEnterMatrixNotFullOrHaveMistakeIsDisabledCalculateButton() {
-        MatrixDiffViewModel viewModel = new MatrixDiffViewModel();
-        viewModel.setSizeOfMatrix("3");
-        viewModel.fillStringImgMatrixConvertToArray("{1,2,3,4,5,6,7,8,A}");
-        assertFalse(viewModel.getIsCalculateButton());
-    }
-    @Test
-    public void whenEnterMatrixNotFullOrHaveMistakeIsEnabledCalculateButton() {
-        MatrixDiffViewModel viewModel = new MatrixDiffViewModel();
-        viewModel.setSizeOfMatrix("3");
-        viewModel.fillStringImgMatrixConvertToArray("{1,2,3,4,5,6,7,8,9}");
-        assertTrue(viewModel.getIsCalculateButton());
-    }
-    @Test
-    public void canCalculateNewArrayOfElements() {
-        MatrixDiffViewModel viewModel = new MatrixDiffViewModel();
-        viewModel.setSizeOfMatrix("3");
-        viewModel.fillStringImgMatrixConvertToArray("{1,2,3,4,5,6,7,8,9}");
-        assertTrue(viewModel.calculateMatrixDiff());
-    }
-    @Test
-    public void canGetResultOfCalculateNewArrayOfElements() {
-        MatrixDiffViewModel viewModel = new MatrixDiffViewModel();
-        viewModel.setSizeOfMatrix("3");
-        viewModel.fillStringImgMatrixConvertToArray("{1,2,3,4,5,6,7,8,9}");
-        viewModel.calculateMatrixDiff();
-        assertEquals(viewModel.getResultOfCalculate(), 0, 0.0001);
-    }
+@Test
+public void whenEnteredCorrectNumericInTextFieldToFillButtonIsEnabled() {
+viewModel.setSizeOfMatrix("3");
+assertTrue(viewModel.isToFillButtonEnabled());
+}
+
+@Test
+public void whenClearTextFieldToFillButtonIsDisabled() {
+viewModel.setSizeOfMatrix("3");
+viewModel.setSizeOfMatrix("");
+assertFalse(viewModel.isToFillButtonEnabled());
+}
+
+@Test
+public void whenEnterRightSizeGetImplementOfMatrix() {
+viewModel.setSizeOfMatrix("3");
+assertNotEquals(viewModel.getEmptyMatrix(), "");
+}
+
+@Test
+public void whenEnterMatrixNotFullOrHaveMistakeIsDisabledCalculateButton() {
+viewModel.setSizeOfMatrix("3");
+viewModel.fillStringImgMatrixConvertToArray("{1,2,3,4,5,6,7,8,A}");
+assertFalse(viewModel.getIsCalculateButton());
+}
+@Test
+public void whenEnterMatrixNotFullOrHaveMistakeIsEnabledCalculateButton() {
+viewModel.setSizeOfMatrix("3");
+viewModel.fillStringImgMatrixConvertToArray("{1,2,3,4,5,6,7,8,9}");
+assertTrue(viewModel.getIsCalculateButton());
+}
+@Test
+public void canCalculateNewArrayOfElements() {
+viewModel.setSizeOfMatrix("3");
+viewModel.fillStringImgMatrixConvertToArray("{1,2,3,4,5,6,7,8,9}");
+assertTrue(viewModel.calculateMatrixDiff());
+}
+@Test
+public void canGetResultOfCalculateNewArrayOfElements() {
+viewModel.setSizeOfMatrix("3");
+viewModel.fillStringImgMatrixConvertToArray("{1,2,3,4,5,6,7,8,9}");
+viewModel.calculateMatrixDiff();
+assertEquals(viewModel.getResultOfCalculate(), 0, 0.0001);
+}
 }
